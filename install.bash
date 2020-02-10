e06d07b0c1d31bae83b1b678e5ac6c9ef9488ecd
Jorge Andres Moreno Mera <jorgemoreno97@hotmail.com>
2020-02-10 01:05:40 -0800

    Add playbook PostgreSQL and fixes playbook mySQL

--- install.bash
+++ install.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo apt install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
 sudo apt install ansible -y
+sudo apt install python3-pip
+sudo apt install python-pip
